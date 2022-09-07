a7c7f1033f8922b6256edeb014eef8e289114620
seanpm1954 <smaloney@netpcpros.com>
2022-09-07 14:17:25 -0500

    add go files

--- contol-flow/flow.go
+++ contol-flow/flow.go
@@ -1,5 +1,48 @@
 package main
 
 func main() {
+	//loop
+	//print even numbers 1-100
+	//for i := 0; i < 100; i++ {
+	//	if i%2 == 0 {
+	//		fmt.Println(i)
+	//	}
+	//}
+	//fmt.Printf("Done at %d\n\n", x)
+	//
+	//for i := 22; i <= 122; i++ {
+	//	asciiValue := int(i)
+	//
+	//	fmt.Printf("Ascii Value of %c = %d\t UTF-8 Value is %U\n", asciiValue, asciiValue, asciiValue)
+	//}
 
+	//if
+	//if true {
+	//	fmt.Println("001")
+	//}
+	//if false {
+	//	fmt.Println("002")
+	//}
+	//if !true {
+	//	fmt.Println("003")
+	//}
+	//if !false {
+	//	fmt.Println("004")
+	//}
+	//var j int
+	//for i := 1954; i < 2023; i++ {
+	//
+	//	fmt.Println(i, j)
+	//	j++
+	//}
+	//for i := 10; i < 101; i++ {
+	//	j := i % 4
+	//	fmt.Println(j)
+	//}
+
+	var favSport string
+
+	switch favSport {
+	
+	}
 }
--- slices/slice.go
+++ slices/slice.go
@@ -1,1 +1,13 @@
-package slices
+package main
+
+import "fmt"
+
+// a SLICE allows you to group VALUES of he same TYPE
+func main() {
+	// x:= type{values} COMPOSITE LITERAL
+	x := []int{4, 5, 7, 8, 42}
+	fmt.Println(x)
+	for _, num := range x {
+		fmt.Println(num)
+	}
+}
