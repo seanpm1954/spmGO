ce09cb115b7bc083ef8379be3733747b34e5d191
seanpm1954 <smaloney@netpcpros.com>
2022-09-10 00:02:09 -0500

    added func and method folders

--- functions/func.go
+++ functions/func.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum(xi...)
+	fmt.Println(x)
+}
+
+func sum(x ...int) int {
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
 
 }
